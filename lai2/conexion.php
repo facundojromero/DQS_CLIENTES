a9b901c25b368fecbdb0169dc9c40daa9ecb914c
facundojromero <facundoj.romero@gmail.com>
2026-04-23 15:20:55 -0300

    Actualizar credenciales de base de datos en lai2

--- lai2/conexion.php
+++ lai2/conexion.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost';
-$user = 'root';
-$password = '';
-$dbname = 'sistema_venta_lai';
+$host = '127.0.0.1';
+$user = 'u385461681_lai_prueba_us';
+$password = 'Curuzu.1810';
+$dbname = 'u385461681_lai_prueba';
 
 $conn = new mysqli($host, $user, $password, $dbname);
 
